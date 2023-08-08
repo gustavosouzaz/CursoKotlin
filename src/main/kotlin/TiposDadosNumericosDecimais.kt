ffb88dce3e2a21ebe372078c6d2682d4a0e25ac1
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-08-08 11:24:26 -0300

    TiposDadosNumericosDecimais

---
+++ src/main/kotlin/TiposDadosNumericosDecimais.kt
@@ -0,0 +1,15 @@
+fun main () {
+
+
+    /*
+    Float: 4 bytes - 32 bits
+    Double: 8 bytes - 64 bits
+     */
+
+
+
+    val d1 = 14.5
+    val d2: Double = 14.5
+    val f1 = 14.5f
+    val f2: Float = 32.5f
+}
