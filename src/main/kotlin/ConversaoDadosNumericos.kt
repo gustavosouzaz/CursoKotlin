6635c498a87817194ea7ed02a9370be9322ce071
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-08-08 11:38:15 -0300

    TiposDadosNumericosDecimais

---
+++ src/main/kotlin/ConversaoDadosNumericos.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val i1 = 10
+    val d1 = i1.toDouble()
+    println(d1)
+
+    val li = i1.toLong()
+    println(li)
+
+    val d2 = 10.34
+    val i2 = d2.toInt()
+    println(i2)
+
+
+
+
+}
