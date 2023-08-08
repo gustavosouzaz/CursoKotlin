41b3654721fdb150a43c7d04323e347acb22b2d8
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-08-08 10:55:31 -0300

    Variáveis

---
+++ src/main/kotlin/Variaveis.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+   // var counter:Int
+    // counter = 10
+
+    // pode fazer assim também
+
+    // var counter: Int = 10
+
+    // Forma mais concisa e mais utlizada
+
+    var counter = 10
+
+
+
+    val counter2 = 10
+   // counter2 = 5  -> impossivel troca de valores para val
+
+}
