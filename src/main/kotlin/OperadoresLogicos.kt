e329f968e841ad06514913525f89bd445e2f2635
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-08-20 19:08:15 -0300

    OperadoresLogicos

---
+++ src/main/kotlin/OperadoresLogicos.kt
@@ -0,0 +1,45 @@
+fun main() {
+
+    //Tabela Verdade
+
+    //AND
+
+    println("T AND T ${true && true}")
+    println("T AND T ${true && false}")
+    println("T AND T ${false && true}")
+    println("T AND T ${false && false}")
+
+    //  println(true && true)
+  //  println(true && false)
+  //  println(false && true)
+  //  println(false && false)
+
+    println()
+
+
+    //OR
+
+    println("T OR T ${true || true}")
+    println("T OR T ${true || false}")
+    println("T OR T ${false || true}")
+    println("T OR T ${false || false}")
+
+    //  println(true && true)
+    //  println(true && false)
+    //  println(false && true)
+    //  println(false && false)
+
+
+     println()
+
+    //NOT
+
+    println("NOT F ${!false}")
+    println("NOT T ${!true}")
+
+    //  println(!false)
+    //  println(!true)
+
+
+
+}
