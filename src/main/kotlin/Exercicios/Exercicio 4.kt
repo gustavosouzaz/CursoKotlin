1fa0437a42e701e2a70a87288a7fc6dddb7c5890
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-08-30 20:35:22 -0300

    Exercicio3

---
+++ src/main/kotlin/Exercicios/Exercicio 4.kt
@@ -0,0 +1,26 @@
+package Exercicios
+
+/**
+ * EXERCÍCIO 4
+ * -----------
+ *
+ * Encontre o resultado da seguinte expressão
+ *
+ * !(2 * 4 >= 16 / 2 && 5 == 4 + 1)
+ *
+ */
+
+fun main() {
+
+val exp = !(2 * 4 >= 16 / 2 && 5 == 4 + 1)
+
+    /**
+     * !(2 * 4 >= 16 / 2 && 5 == 4 + 1)
+     * !(8 >= 8 && 5 == 5)
+     * !(T && T)
+     * !T
+     * F
+     */
+
+print(exp)
+}
