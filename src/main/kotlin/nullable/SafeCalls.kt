87975669c4ecce8c8272448748f6da2eb0dd837a
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-09-22 19:11:30 -0300

    SafeCalls outro jeito

--- src/main/kotlin/nullable/SafeCalls.kt
+++ src/main/kotlin/nullable/SafeCalls.kt
@@ -3,16 +3,19 @@ package nullable
 fun main() {
 
     val s1: String? = "abcde"
-    println(s1)
 
-    val s2: String? = s1?.uppercase()
-    println(s2)
+    if(s1 != null) {
 
-    val s3: String? = s2?.reversed()
-    println(s3)
+        val s2: String = s1.uppercase()
+        println(s2)
+
+        val s3: String = s2.reversed()
+        println(s3)
 
 
-    val s4 = s3?.length
-    println(s4)
+        val s4 = s3.length
+        println(s4)
+    }
+
 
 }
