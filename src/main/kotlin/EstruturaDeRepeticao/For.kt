bafd0915cbeae267dec3955913ec9e42992a7613
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-09-21 22:29:26 -0300

    FOR

---
+++ src/main/kotlin/EstruturaDeRepeticao/For.kt
@@ -0,0 +1,27 @@
+package EstruturaDeRepeticao
+
+fun main() {
+
+
+    for (i in 0..10) {
+
+        print("$i ")
+
+    }
+println()
+
+    for(n in 10 downTo 0 step 2 ) {
+
+        print("$n ")
+    }
+
+
+    println()
+    for (x in 10 until 100 step 3) {
+
+        print("$x ")
+
+    }
+
+
+}
