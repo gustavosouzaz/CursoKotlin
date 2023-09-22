b6d682fbbacdc75d9d97e4788eb02f1ae26acf96
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-09-21 21:59:03 -0300

    Estrutura de Repetição WHILE

--- src/main/kotlin/EstruturaDeRepeticao/While.kt
+++ src/main/kotlin/EstruturaDeRepeticao/While.kt
@@ -2,24 +2,16 @@ package EstruturaDeRepeticao
 
 fun main() {
 
-    var x = 0
+    var x = 10
 
-    while (x <= 10) {
+    while (x < 5) {
 
         print("$x ")
-
-        x++
-
     }
 
-println()
-     x = 0
-   do {
-       print("$x ")
+    do {
+        print("$x ")
 
-       x++
-
-   }while (x <= 10)
-
+    }while (x < 5)
 
 }
