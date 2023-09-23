3e7f7b5621894afe2b3695826b22f1a53c3f5d8d
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-09-22 22:06:52 -0300

    elvisoperator

---
+++ src/main/kotlin/nullable/elvisOperator.kt
@@ -0,0 +1,16 @@
+package nullable
+
+fun main () {
+
+   val value: Int? = 10
+
+    val value2 = if (value != null) value else 0
+    println(value2)
+
+    val value3 = value ?: 0
+
+    println(value3)
+
+
+
+}
